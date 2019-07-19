12a276dee40a7fa1a912654e8da197c3e4819749
Chizi123 <joelgrun@gmail.com>
2019-07-19 18:52:48 +1000

    Building and commiting package on add

--- add_package.sh
+++ add_package.sh
@@ -2,5 +2,18 @@
 
 CHDIR=$(dirname "$(realpath $0)")/chroot 
 mount --bind $CHDIR $CHDIR
-$CHDIR/bin/arch-chroot $CHDIR su joel -c "cd /build/repo; git clone $1"
+$CHDIR/bin/arch-chroot $CHDIR su joel -c "cd /build/repo;
+					   		  	 	  	  git clone http://aur.archlinux.org/$1.git
+										  cd $1;
+										  makepkg -s --noconfirm;
+										  ln $1-*.pkg.tar.xz ../x86_64/;
+										  cd ../x86_64;
+										  rm Chizi123.db Chizi123.files;
+										  repo-add ../Chizi123.db.tar.xz $1-*.pkg.tar.xz;
+										  ln ../Chizi123.db.tar.xz Chizi123.db;
+										  ln ../Chizi123.files.tar.xz Chizi123.files;
+										  cd ../;
+										  git add x86_64/;
+										  git commit -m \"added $1\";
+										  git push"
 umount $CHDIR
