41117a7975234976d991ccad151e831614c04e75
Chizi123 <joelgrun@gmail.com>
2019-08-23 11:28:57 +1000

    added script to add package as dependency

---
+++ add_dependency.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CHDIR=$(dirname "$(realpath $0)")/chroot 
+mount --bind $CHDIR $CHDIR
+$CHDIR/bin/arch-chroot $CHDIR su joel -c "cd /build/repo/dependencies;
+					   		  	 	  	  git clone http://aur.archlinux.org/$1.git
+										  cd $1;
+										  makepkg -si --noconfirm;
+										  ln $1-*.pkg.tar.xz ../../x86_64/;
+										  cd ../../x86_64;
+										  rm Chizi123.db Chizi123.files;
+										  repo-add ../Chizi123.db.tar.xz $1-*.pkg.tar.xz;
+										  ln ../Chizi123.db.tar.xz Chizi123.db;
+										  ln ../Chizi123.files.tar.xz Chizi123.files;
+										  cd ../;
+										  git add x86_64/;
+										  git commit -m \"added $1\";
+										  git push"
+umount $CHDIR
