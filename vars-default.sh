a2d95e38d1bbaf3b84f4a725412e4f6bd7de206d
Joel Grunbaum <joelgrun@gmail.com>
2020-10-15 17:59:18 +1100

    reordered variables, hopefully makes more sense

--- vars-default.sh
+++ vars-default.sh
@@ -1,12 +1,12 @@
 REPODIR=/repo/x86_64
 BUILDDIR=/repo/build
 REPONAME=
+export PACKAGER="John Doe <jd@change.me>"
+EMAIL=""
 UPDATE=N
+PARALLEL=N
+QUIET=N
 CLEAN=N
 SIGN=N
 KEY=""
 NUM_OLD=5
-export PACKAGER="John Doe <jd@change.me>"
-EMAIL=""
-PARALLEL=N
-QUIET=N
